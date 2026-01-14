79c0f46254113fee2f104d104a67db7f97ff99ef
Festiveokagbare <festiveokotieboh@gmail.com>
2026-01-14 22:50:40 +0100

    React and build caching Github page deployment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>🚀 CI/CD React Demo</h1>
+      <p>Deployed automatically using GitHub Actions.</p>
+    </div>
+  );
+}
+
+export default App;
